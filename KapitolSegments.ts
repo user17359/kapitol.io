deec2197c31ee28ad2438af163be4dd298902c20
Ryszard Błażej <sapermysza@gmail.com>
2024-12-09 14:24:42 +0100

    First working physics

--- KapitolSegments.ts
+++ KapitolSegments.ts
@@ -37,30 +37,33 @@ export class KapitolSegments{
 
 
         // Shared box geometry
-        const centralBoxGeometry = new THREE.BoxGeometry(2, 1, 5);
-        const sideBoxGeometry = new THREE.BoxGeometry(2, 1, 6);
+        const centralBoxGeometry = new THREE.BoxGeometry(1, 1, 2);
+        const sideBoxGeometry = new THREE.BoxGeometry(1, 1, 3);
 
         // Create central box mesh
         const centralBoxMesh = new THREE.Mesh(centralBoxGeometry, m);
-        centralBoxMesh.scale.set(0.5, 0.5, 0.5);
         centralBoxMesh.rotateY(Math.PI);
         meshes.push(centralBoxMesh);
 
         // Create left box mesh
         const leftBoxMesh = new THREE.Mesh(sideBoxGeometry, m);
-        leftBoxMesh.position.set(1, 0, 0); // Use position.set instead of translateX
-        leftBoxMesh.scale.set(0.5, 0.5, 0.5);
+        leftBoxMesh.position.set(1, 0, 0)
         leftBoxMesh.rotateY(Math.PI);
         meshes.push(leftBoxMesh);
 
         // Create right box mesh
         const rightBoxMesh = new THREE.Mesh(sideBoxGeometry, m);
-        rightBoxMesh.position.set(-1, 0, 0); // Use position.set instead of translateX
-        rightBoxMesh.scale.set(0.5, 0.5, 0.5);
+        rightBoxMesh.position.set(-1, 0, 0);
         rightBoxMesh.rotateY(Math.PI);
         meshes.push(rightBoxMesh);
 
         return meshes;
+    }
+
+    repositionSegments(meshes: THREE.Object[], vector: { x: number; y: number; z: number; }){
+        meshes[0].position.set(vector.x, vector.y, vector.z);
+        meshes[1].position.set(vector.x + 1, vector.y, vector.z);
+        meshes[2].position.set(vector.x - 1, vector.y, vector.z);
     }
 
 }
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { segments, KapitolSegments } from './KapitolSegments';
 import { OutlineEffect } from 'three/addons/effects/OutlineEffect.js';
+import RAPIER from 'https://cdn.skypack.dev/@dimforge/rapier3d-compat';
 
 const kapitolSegments = new KapitolSegments();
 
@@ -13,35 +14,115 @@ directionalLight.position.set(2, 8, 4);
 scene.add(directionalLight);
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let cameraYChange = 0.0;
+const cameraTick = 0.05;
+const cameraMove = 0.75;
+
+let spawnPoint = {"x": 0, "y": 7.0, "z":0};
+const spawnPointMove = 0.75;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
+
+let world: RAPIER.World;
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == 88 && world != undefined) {
+        createNewSegment(world)
+    }
+};
+
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.PlaneGeometry( 15, 15 );
 const material = new THREE.MeshToonMaterial( {color: 0x447C00, side: THREE.DoubleSide} ); 
 const plane = new THREE.Mesh( geometry, material );
 plane.rotateX(Math.PI/2)
-plane.translateZ(0.4)
+plane.translateZ(0.5)
 scene.add( plane );
 
-const objects = kapitolSegments.getSegment(segments.BASIC);
-objects.forEach((obj) => scene.add(obj)) 
-const objects2 = kapitolSegments.getSegment(segments.BASIC);
-objects2.forEach((obj) => {obj.translateY(2); scene.add(obj)}) 
+let segmentsOnBoard: Array<Segment> = [];
 
 // isometric camera
-let distance = 5;
+let distance = 7;
 camera.position.set( distance, distance, distance );
 camera.lookAt( scene.position );
+camera.translateY(3)
 
 let effect = new OutlineEffect( renderer );
 
 function animate() {
-	
+	if(cameraYChange > 0){
+		camera.translateY(cameraTick);
+		camera.translateZ(cameraTick);
+		cameraYChange -= cameraTick;
+	}
 }
 
 renderer.setAnimationLoop(() => {
 	animate()
 	effect.render(scene, camera);
 })
+
+RAPIER.init().then(() => {
+
+    // Use the RAPIER module here.
+    let gravity = { x: 0.0, y: -9.81, z: 0.0 };
+
+	world = new RAPIER.World(gravity);
+
+    // Create the ground
+    let groundColliderDesc = RAPIER.ColliderDesc.cuboid(10.0, 0.5, 10.0)
+	.setTranslation(0.0, -0.5, 0.0);
+    world.createCollider(groundColliderDesc);
+
+	// Create the 0 segment
+	segmentsOnBoard.push(new Segment(world, {"x": 0, "y": 0.5, "z":0}));
+
+	// Create the 1 segment
+	segmentsOnBoard.push(new Segment(world, spawnPoint));
+    
+	let gameLoop = () => {
+		// Step the simulation forward.  
+		world.step();
+	
+		segmentsOnBoard.forEach((seg) => {seg.update()}) 
+	
+		setTimeout(gameLoop, 16);
+	};
+
+	gameLoop();
+});
+
+function createNewSegment(world: RAPIER.world){
+	segmentsOnBoard.push(new Segment(world, spawnPoint));
+	cameraYChange += cameraMove;
+	spawnPoint.y += 1;
+}
+
+class Segment{
+	meshes: THREE.Object[]
+	rigidbody: RAPIER.RigidBody;
+
+	constructor (world: RAPIER.World, position:{ x: number; y: number; z: number; }) {
+		this.meshes = kapitolSegments.getSegment(segments.BASIC);
+		this.meshes.forEach((obj) => {scene.add(obj)}) 
+
+		 let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
+		 .setTranslation(position.x, position.y, position.z);
+		this.rigidbody = world.createRigidBody(rigidBodyDesc);
+	
+		let colliderDesc = RAPIER.ColliderDesc.cuboid(3.0, 0.5, 3.0);
+		world.createCollider(colliderDesc, this.rigidbody);
+	}
+
+	update() {
+		if(this.rigidbody != undefined){
+			let pos = this.rigidbody.translation();
+			kapitolSegments.repositionSegments(this.meshes, {"x": pos.x, "y": pos.y, "z": pos.z}) ;
+		}
+	}
+	
+}
