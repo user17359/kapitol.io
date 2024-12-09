b1bd5e50d91f71787a64e62f73a14e32b360310c
Ryszard Błażej <sapermysza@gmail.com>
2024-12-09 17:29:19 +0100

    Working oscilating move

--- KapitolSegments.ts
+++ KapitolSegments.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
 export enum segments {
     BABILON = 0,
@@ -35,10 +36,23 @@ export class KapitolSegments{
             visible: true
        };
 
+       let mGreen = new THREE.MeshToonMaterial(
+        {
+            color: 0x34AF65,
+            gradientMap: this.gradientTexture
+        }
+        )
+        mGreen.userData.outlineParameters = {
+            thickness: 0.01,
+            color: new THREE.Color().setHSL(0, 0, 0).toArray(),
+            alpha: 1,
+            visible: true
+    };
+
 
         // Shared box geometry
         const centralBoxGeometry = new THREE.BoxGeometry(1, 1, 2);
-        const sideBoxGeometry = new THREE.BoxGeometry(1, 1, 3);
+        const sideBoxGeometry = new THREE.BoxGeometry(2, 1, 3);
 
         // Create central box mesh
         const centralBoxMesh = new THREE.Mesh(centralBoxGeometry, m);
@@ -47,23 +61,28 @@ export class KapitolSegments{
 
         // Create left box mesh
         const leftBoxMesh = new THREE.Mesh(sideBoxGeometry, m);
-        leftBoxMesh.position.set(1, 0, 0)
+        leftBoxMesh.position.set(1.5, 0, 0)
         leftBoxMesh.rotateY(Math.PI);
         meshes.push(leftBoxMesh);
 
         // Create right box mesh
-        const rightBoxMesh = new THREE.Mesh(sideBoxGeometry, m);
-        rightBoxMesh.position.set(-1, 0, 0);
+        let rightBoxMesh: THREE.Mesh;
+        if(segment == segments.GREEN) rightBoxMesh = new THREE.Mesh(sideBoxGeometry, mGreen);
+        else rightBoxMesh = new THREE.Mesh(sideBoxGeometry, m);
+        rightBoxMesh.position.set(-1.5, 0, 0);
         rightBoxMesh.rotateY(Math.PI);
         meshes.push(rightBoxMesh);
 
         return meshes;
     }
 
-    repositionSegments(meshes: THREE.Object[], vector: { x: number; y: number; z: number; }){
+    repositionSegments(meshes: THREE.Object[], vector: { x: number; y: number; z: number; }, rotation: THREE.Quaternion){
         meshes[0].position.set(vector.x, vector.y, vector.z);
-        meshes[1].position.set(vector.x + 1, vector.y, vector.z);
-        meshes[2].position.set(vector.x - 1, vector.y, vector.z);
+        meshes[0].setRotationFromQuaternion(rotation);
+        meshes[1].position.set(vector.x + 1.5, vector.y, vector.z);
+        meshes[1].setRotationFromQuaternion(rotation)
+        meshes[2].position.set(vector.x - 1.5, vector.y, vector.z);
+        meshes[2].setRotationFromQuaternion(rotation)
     }
 
 }
--- main.ts
+++ main.ts
@@ -18,7 +18,7 @@ let cameraYChange = 0.0;
 const cameraTick = 0.05;
 const cameraMove = 0.75;
 
-let spawnPoint = {"x": 0, "y": 7.0, "z":0};
+let spawnPoint = {"x": 0, "y": 8.0, "z":0};
 const spawnPointMove = 0.75;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -36,7 +36,7 @@ function onDocumentKeyDown(event) {
 
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.PlaneGeometry( 15, 15 );
+const geometry = new THREE.PlaneGeometry( 20, 20 );
 const material = new THREE.MeshToonMaterial( {color: 0x447C00, side: THREE.DoubleSide} ); 
 const plane = new THREE.Mesh( geometry, material );
 plane.rotateX(Math.PI/2)
@@ -74,12 +74,13 @@ RAPIER.init().then(() => {
 	world = new RAPIER.World(gravity);
 
     // Create the ground
-    let groundColliderDesc = RAPIER.ColliderDesc.cuboid(10.0, 0.5, 10.0)
+    let groundColliderDesc = RAPIER.ColliderDesc.cuboid(20.0, 0.5, 20.0)
 	.setTranslation(0.0, -0.5, 0.0);
     world.createCollider(groundColliderDesc);
 
 	// Create the 0 segment
 	segmentsOnBoard.push(new Segment(world, {"x": 0, "y": 0.5, "z":0}));
+	segmentsOnBoard[0].startGravity();
 
 	// Create the 1 segment
 	segmentsOnBoard.push(new Segment(world, spawnPoint));
@@ -96,32 +97,81 @@ RAPIER.init().then(() => {
 	gameLoop();
 });
 
+let segmentID = 1;
 function createNewSegment(world: RAPIER.world){
-	segmentsOnBoard.push(new Segment(world, spawnPoint));
+
+	segmentsOnBoard[segmentsOnBoard.length - 1].startGravity();
 	cameraYChange += cameraMove;
 	spawnPoint.y += 1;
+	segmentID += 1;
+
+	setTimeout(function(){
+		if(segmentID == 3){
+			segmentsOnBoard.push(new Segment(world, spawnPoint, segments.GREEN));
+		}
+		else if(segmentID == 5){
+			segmentsOnBoard.push(new Segment(world, spawnPoint, segments.NAME));
+		}
+		else{
+			if(segmentID < 15){
+				segmentsOnBoard.push(new Segment(world, spawnPoint, segments.BASIC));
+			}
+			else{
+
+			}
+		}
+	}, 1500);
 }
+
+let oscilatorBound = 3;
+let oscilatorStep = 0.05;
 
 class Segment{
 	meshes: THREE.Object[]
 	rigidbody: RAPIER.RigidBody;
+	oscilating = true; 
+	oscilatorValue = 0;
+	oscialtorDir = true;
 
-	constructor (world: RAPIER.World, position:{ x: number; y: number; z: number; }) {
-		this.meshes = kapitolSegments.getSegment(segments.BASIC);
+	constructor (world: RAPIER.World, position:{ x: number; y: number; z: number; }, segment: segments) {
+		this.meshes = kapitolSegments.getSegment(segment);
 		this.meshes.forEach((obj) => {scene.add(obj)}) 
 
 		 let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
 		 .setTranslation(position.x, position.y, position.z);
 		this.rigidbody = world.createRigidBody(rigidBodyDesc);
+
+		this.rigidbody.setGravityScale(0.0);
 	
-		let colliderDesc = RAPIER.ColliderDesc.cuboid(3.0, 0.5, 3.0);
+		let colliderDesc = RAPIER.ColliderDesc.cuboid(4.0, 0.5, 3.0).setDensity(10.0);
 		world.createCollider(colliderDesc, this.rigidbody);
 	}
 
+	startGravity() {
+		this.rigidbody.setGravityScale(1.0);
+		this.oscilating = false;
+	}
+
 	update() {
+		let pos = this.rigidbody.translation();
+		let rot = this.rigidbody.rotation();
+		if(this.oscilating){
+			this.rigidbody.setLinvel({ x: 0, y: 0, z: this.oscilatorValue })
+			if(this.oscialtorDir && this.oscilatorValue < oscilatorBound){
+				this.oscilatorValue += oscilatorStep;
+			}
+			else if(this.oscialtorDir && this.oscilatorValue >= oscilatorBound){
+				this.oscialtorDir = !this.oscialtorDir
+			}
+			if(!this.oscialtorDir && this.oscilatorValue > -oscilatorBound){
+				this.oscilatorValue -= oscilatorStep;
+			}
+			else if(!this.oscialtorDir && this.oscilatorValue <= -oscilatorBound){
+				this.oscialtorDir = !this.oscialtorDir
+			}
+		}
 		if(this.rigidbody != undefined){
-			let pos = this.rigidbody.translation();
-			kapitolSegments.repositionSegments(this.meshes, {"x": pos.x, "y": pos.y, "z": pos.z}) ;
+			kapitolSegments.repositionSegments(this.meshes, {"x": pos.x, "y": pos.y, "z": pos.z}, new THREE.Quaternion(rot.x, rot.y, rot.z, rot.w)) ;
 		}
 	}
 	
